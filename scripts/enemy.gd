161f6f9f2fd023afee687d7e0a9ac510b384b850
Alston <alstonl17@nycstudents.net>
2026-06-08 11:09:17 -0400

    Trying to add sideways platform again.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,9 +5,13 @@ var speed = 50
 
 var direction = "right"
 
+var switching_direction = false
+
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_infront_bottom: RayCast2D = $RayCastInfrontBottom
+@onready var ray_cast_bottom: RayCast2D = $RayCastBottom
+@onready var timer: Timer = $Timer
 
 func _process(delta: float):
 	if direction == "right":
@@ -20,11 +24,18 @@ func _process(delta: float):
 		if ray_cast_left.is_colliding():
 			direction = "right"
 	
-	if not ray_cast_infront_bottom.is_colliding():
+	if not ray_cast_bottom.is_colliding() and switching_direction == false:
 		if direction == "right":
 			direction = "left"
+			switching_direction = true;
+		else:
+			switching_direction = false;
 		
 		if direction == "left":
 			direction = "right"
+			switching_direction = true;
+		else:
+			switching_direction = false;
+			
 	
 	
